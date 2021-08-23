b150de2d69527d91db7a99a98f915c1941125085
meavv <c.c.c.p@bk.ru>
2021-08-23 15:51:06 +0500

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -0,0 +1,88 @@
+package ru.job4j.collection.list;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private int modCount;
+    private int size;
+    private Node<E> last;
+    private Node<E> first;
+
+    public SimpleLinkedList() {
+        last = new Node<E>(null, first, null);
+        first = new Node<E>(null, null, last);
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        public Node(E item, Node<E> prev, Node<E> next) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public E getItem() {
+            return item;
+        }
+
+        public void setItem(E item) {
+            this.item = item;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        Node<E> temp = last;
+        temp.setItem(value);
+        last = new Node<E>(null, temp, null);
+        temp.setNext(last);
+        size++;
+        modCount++;
+    }
+
+
+    @Override
+    public E get(int index) {
+        index = Objects.checkIndex(index, size);
+        Node<E> rsl = first.getNext();
+        for (int i = 0; i < index; i++) {
+            rsl = rsl.getNext();
+        }
+        return rsl.getItem();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int it = 0;
+            final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return it < size;
+            }
+
+            @Override
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return get(it++);
+            }
+        };
+    }
+}
---
+++ src/test/java/ru/job4j/collection/list/SimpleLinkedListTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.collection.list;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class SimpleLinkedListTest {
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void test() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        assertThat(list.get(5), Is.is(6));
+        assertThat(list.get(9), Is.is(2));
+    }
+
+    @Test
+    public void whenAddAndGet() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        assertThat(list.get(0), Is.is(1));
+        assertThat(list.get(1), Is.is(2));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetFromOutOfBoundThenExceptionThrown() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.get(2);
+    }
+
+    @Test
+    public void whenGetIteratorTwiceThenEveryFromBegin() {
+        List<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+
+        Iterator<Integer> first = list.iterator();
+        assertThat(first.hasNext(), Is.is(true));
+        assertThat(first.next(), Is.is(1));
+        assertThat(first.hasNext(), Is.is(true));
+        assertThat(first.next(), Is.is(2));
+        assertThat(first.hasNext(), Is.is(false));
+
+        Iterator<Integer> second = list.iterator();
+        assertThat(second.hasNext(), Is.is(true));
+        assertThat(second.next(), Is.is(1));
+        assertThat(second.hasNext(), Is.is(true));
+        assertThat(second.next(), Is.is(2));
+        assertThat(second.hasNext(), Is.is(false));
+    }
+
+}
