bb7e0e8b6625635dab7cdf9863aae007bebb3a1a
meavv <c.c.c.@bk.ru>
2021-09-15 11:20:19 +0500

    2. Анализ доступности сервера. [#859 #202625]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,28 +5,23 @@ import java.util.*;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> list = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(
                      new BufferedOutputStream(new FileOutputStream(target)))
         ) {
             String line;
-            while ((line = br.readLine()) != null) {
-                list.add(line);
-            }
             String first = null;
-            String last = null;
-            for (String s1 : list) {
-                if (first == null && (s1.startsWith("400") || s1.startsWith("500"))) {
-                    first = s1;
+            String last;
+            while ((line = br.readLine()) != null) {
+                if (first == null && (line.startsWith("400") || line.startsWith("500"))) {
+                    first = line;
                     out.print(first.substring(4));
-                } else if (first != null && (!s1.startsWith("400") && !s1.startsWith("500"))) {
-                    last = s1;
+                } else if (first != null && (!line.startsWith("400") && !line.startsWith("500"))) {
+                    last = line;
                     out.println(" ; " + last.substring(4) + ";");
                     first = null;
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
