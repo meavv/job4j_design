ec8ae041c162c6f52eef1edfd0d20fbe44eb1825
meavv <c.c.c.@bk.ru>
2021-09-22 11:46:25 +0500

    5. Валидация параметров запуска. [#246865 #206135]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,6 +1,5 @@
 package ru.job4j.io;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,17 +9,15 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+        validation(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1].substring(2))).forEach(System.out::println);
+    }
+
+    public static void validation(String[] args) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
-        if (!Files.exists(Path.of(args[0]))) {
-            throw new IOException("Папка не найдена");
-        }
-        if (args.length < 2) {
-            throw new ArrayIndexOutOfBoundsException("Нужно ввести 2 параметра");
-        }
-        Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
