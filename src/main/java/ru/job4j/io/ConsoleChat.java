8e484beb67ff011bc66127ff5a22a54e450d39cb
meavv <c.c.c.p@bk.ru>
2021-09-29 12:38:00 +0500

    6. Кодировка. [#862 #209071]

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -6,13 +6,13 @@ import java.util.stream.Collectors;
 
 public class ConsoleChat {
 
-    private static boolean b = true;
     private final String path;
     private final String botAnswers;
     private static final String OUT = "закончить";
     private static final String STOP = "стоп";
     private static final String CONTINUE = "продолжить";
     List<String> logList = new ArrayList<>();
+    List<String> dialog = new ArrayList<>();
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
@@ -20,39 +20,42 @@ public class ConsoleChat {
     }
 
     public void run() {
-        Random random = new Random();
+        readPhrases();
+        try (Scanner scanner = new Scanner(System.in)) {
+            String input = scanner.nextLine();
+            boolean b = true;
+            logList.add(input);
+            while (!input.equals(OUT)) {
+                if (input.equals(STOP)) {
+                    b = false;
+                }
+                if (input.equals(CONTINUE)) {
+                    b = true;
+                }
+                if (b) {
+                    outPrint();
+                }
+                input = scanner.nextLine();
+                logList.add(input);
+            }
+            saveLog();
+        }
+    }
+
+    private void outPrint() {
+        var s = dialog.get(new Random().nextInt(dialog.size()));
+        logList.add(s);
+        System.out.println(s);
+    }
+
+    private void readPhrases() {
         List<String> list = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(botAnswers))) {
             list = br.lines().collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String temp = list.get(random.nextInt(list.size()));
-        logList.add(temp);
-        System.out.println(temp);
-        readPhrases();
-    }
-
-    private List<String> readPhrases() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        logList.add(input);
-        if (input.equals(OUT)) {
-            saveLog();
-            System.exit(0);
-
-        }
-        if (input.equals(STOP)) {
-            b = false;
-        }
-        if (input.equals(CONTINUE)) {
-            b = true;
-        }
-        if (b) {
-            run();
-        }
-        readPhrases();
-        return logList;
+        dialog = list;
     }
 
     private void saveLog() {
@@ -68,6 +71,6 @@ public class ConsoleChat {
 
     public static void main(String[] args) {
         ConsoleChat cc = new ConsoleChat("data/logDialog.log", "data/dialog.txt");
-        cc.readPhrases();
+        cc.run();
     }
 }
