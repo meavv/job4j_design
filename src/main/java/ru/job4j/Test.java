4faac6dba06893e4b0b0a8bec590b8b155e1639e
meavv <c.c.c.p@bk.ru>
2021-10-06 13:49:33 +0500

    2. Поиск файлов по критерию [#783 #212303]

--- src/main/java/ru/job4j/Test.java
+++ src/main/java/ru/job4j/Test.java
@@ -4,14 +4,16 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(new BufferedReader(new FileReader("data/tiger.xml")));
-        StringBuilder xml = new StringBuilder();
-        while (sc.hasNext()) {
-            xml.append(sc.next());
-        }
-        System.out.println(xml);
+        String s = "tring";
+        Pattern pattern = Pattern.compile(s + "\\*?");
+        Matcher matcher1 = pattern.matcher("emptyString");
+        System.out.println(matcher1.find());
+
+
     }
 }
--- src/main/java/ru/job4j/search/Find.java
+++ src/main/java/ru/job4j/search/Find.java
@@ -9,29 +9,53 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Find {
+
     public static void main(String[] args) throws IOException {
-        validation(args);
-        Predicate<Path> predicate;
         ArgsName argsName = ArgsName.of(args);
+        validation(argsName);
         Path start = Paths.get(argsName.get("d"));
-        if (argsName.get("t").equals("mask")) {
-            predicate = p -> p.toFile().getName()
-                    .endsWith(argsName.get("n").substring(1));
-        } else {
-            predicate = p -> p.toFile().getName()
-                    .equals(argsName.get("n"));
-        }
-        save(search(start, predicate), argsName.get("o"));
+        save(search(start, predicate(argsName)), argsName.get("o"));
     }
 
-    public static void validation(String[] args) {
-        if (args.length != 4) {
+    public static void validation(ArgsName argsName) {
+        if (argsName.getSize() != 4) {
             throw new IllegalArgumentException("Нужно ввести 4 аргумента");
         }
+        if (argsName.get("d") == null) {
+            throw new IllegalArgumentException("Не указана директория");
+        }
+        if (argsName.get("n") == null) {
+            throw new IllegalArgumentException("Не указано имя файла, маска");
+        }
+        if (argsName.get("t") == null) {
+            throw new IllegalArgumentException("Не указан тип поиска");
+        }
+        if (argsName.get("o") == null) {
+            throw new IllegalArgumentException("Не указано куда записать файл");
+        }
+    }
+
+    public static Predicate<Path> predicate(ArgsName argsName) {
+        String typeSearch = argsName.get("n");
+        if (argsName.get("t").equals("mask")) {
+            return p -> {
+                Pattern pattern = Pattern.compile(typeSearch + "*?");
+                Matcher matcher = pattern.matcher(p.toFile().getName());
+                return matcher.find();
+            };
+        }
+        if (argsName.get("t").equals("name")) {
+            return p -> p.toFile().getName()
+                    .equals(typeSearch);
+        }
+        return null;
     }
 
     public static void save(List<Path> log, String file) {
