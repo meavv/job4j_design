33621c59fc661238854456020edb16d55e66f364
meavv <c.c.c.p@bk.ru>
2021-10-06 17:56:57 +0500

    ci bot

--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -4,12 +4,16 @@ public class SimpleStack<T> {
 
     private ForwardLinked<T> linked = new ForwardLinked<T>();
 
-    // Метод pop() - должен возвращать значение и удалять его из коллекции.
+    /**
+     * Метод pop() - должен возвращать значение и удалять его из коллекции.
+     **/
     public T pop() {
         return (T) linked.deleteFirst();
     }
 
-    // Метод push(T value) - помещает значение в коллекцию.
+    /**
+     * Метод push(T value) - помещает значение в коллекцию.
+     **/
     public void push(T value) {
         linked.addFirst(value);
     }
