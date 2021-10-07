f29e6cd5a17bd8cbec1c3386fdbc2b49436c8261
meavv <c.c.c.p@bk.ru>
2021-10-07 18:50:08 +0500

    Select

---
+++ psql/Tracker/Select.sql
@@ -0,0 +1,7 @@
+select * from fauna where name = 'fish'
+
+select * from fauna where avg_age between 10000 and 21000
+
+select * from fauna where discovery_date is null
+
+select * from fauna where discovery_date < '01.01.1950'
