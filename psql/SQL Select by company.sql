6ba7fadc1785c3b8cc961f41fb5f5328c6c049ee
meavv <c.c.c.@bk.ru>
2021-10-20 20:15:52 +0500

    2. SQL Select by company [#2760 #218419]

--- psql/SQL Select by company.sql
+++ psql/SQL Select by company.sql
@@ -2,7 +2,12 @@ where company_id != '5'
 
 select person.name ,company_id, company.name from person join company on person.company_id = company.id
 
-select company.name, count(person.name) as countPerson from company join person on person.company_id = company.id
-group by company.name
-order by countPerson DESC
-LIMIT 1
+SELECT company.name, count(person.name) as count
+FROM company  join person on person.company_id = company.id
+GROUP BY company.name
+having  count(person.name) in (
+SELECT count(person.name) as count
+FROM company  join person on person.company_id = company.id
+GROUP BY company.name
+	limit 1
+)
