322d729379f4aad3575c241acad8f7781ca5901e
meavv <c.c.c.p@bk.ru>
2021-10-26 15:33:23 +0500

    2. SQL Select by company [#2760 #218419]

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        T r = null;
+        T r = value.get(0);
         for (T t : value) {
             if (comparator.compare(r, t) < 0) {
                 r = t;
---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.kiss;
+
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class MaxMinTest {
+
+    class T {
+
+    }
+
+    @Test
+    public void max() {
+        List<Integer> list = List.of(1,2,3,4,5,500);
+        Comparator<Integer> comparator = Comparator.naturalOrder();
+        MaxMin m = new MaxMin();
+        int r = m.max(list, comparator);
+        assertEquals(r, 500);
+    }
+
+
+    @Test
+    public void min() {
+        List<Integer> list = List.of(1,2,3,4,5,500);
+        Comparator<Integer> comparator = Comparator.naturalOrder();
+        MaxMin m = new MaxMin();
+        int r = m.min(list, comparator);
+        assertEquals(r, 1);
+    }
+}
