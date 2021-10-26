ed03aed5142cc64f4a9362e9860b4239a6c27297
meavv <c.c.c.p@bk.ru>
2021-10-26 15:49:32 +0500

    2. Что такое TDD? [#4918 #220401]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,9 +1,9 @@
 package ru.job4j.tdd;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import org.hamcrest.Matchers;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -25,6 +25,29 @@ public class CinemaTest {
     }
 
     @Ignore
+    @Test (expected = Exception.class)
+    public void buyWhenInvalidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(1986, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
+    @Test (expected = Exception.class)
+    public void buyWhenPlaceIsTaken() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(1986, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
     @Test
     public void find() {
         Cinema cinema = new Cinema3D();
@@ -35,11 +58,30 @@ public class CinemaTest {
 
     @Ignore
     @Test
+    public void findWhenNoSession() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Matchers.nullValue()));
+    }
+
+    @Ignore
+    @Test
     public void add() {
         Cinema cinema = new Cinema3D();
         Session3D session3D = new Session3D();
         cinema.add(session3D);
         List<Session> sessions = cinema.find(session -> true);
         assertTrue(sessions.contains(session3D));
+    }
+
+    @Ignore
+    @Test
+    public void addAlreadyAdd() {
+        Cinema cinema = new Cinema3D();
+        Session3D session3D = new Session3D();
+        cinema.add(session3D);
+        List<Session> sessions = cinema.find(session -> true);
+        assertFalse(sessions.contains(session3D));
     }
 }
