01d99cfb9da787db494e3165d77a8070914cf390
meavv <c.c.c.p@bk.ru>
2021-10-27 17:52:01 +0500

    123

--- src/main/java/ru/job4j/template/Gen.java
+++ src/main/java/ru/job4j/template/Gen.java
@@ -1,10 +1,35 @@
 package ru.job4j.template;
 
+import ru.job4j.serialization.java.A;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Gen implements Generator {
+
     @Override
-    public String produce(String template, Map<String, String> args) {
-        return null;
+    public String produce(String template, Map<String, String> args) throws Exception {
+        var list = Arrays.stream(template.split(" "))
+                .filter(a -> a.contains("${"))
+                .map(a -> a.replaceAll("\\$|\\{|}|,|\\?", ""))
+                .collect(Collectors.toList());
+        String rsl = template.replaceAll("\\$|\\{|}", "");
+        if (args.size() != list.size()) {
+            throw new Exception();
+        }
+        for (String s : list) {
+            rsl = rsl.replace(s, args.get(s));
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Map<String, String> map = Map.of("name", "Qwe", "subject", "you");
+        Generator g = new Gen();
+        String template = "I am a ${name}, Who are ${subject}?";
+        var x = g.produce(template, map);
+        System.out.println(x);
     }
 }
