461bf0b189a722c0a535b54edacf7ef5b7938b03
meavv <c.c.c.p@bk.ru>
2021-10-27 13:14:54 +0500

    3. Шаблонизатор. [#855 #220479]

--- src/test/java/ru/job4j/template/GenTest.java
+++ src/test/java/ru/job4j/template/GenTest.java
@@ -11,7 +11,7 @@ public class GenTest {
 
     @Ignore
     @Test
-    public void produce() {
+    public void produce() throws Exception {
         Generator generator = new Gen();
         String s = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> map = Map.of("name", "Qwe", "subject", "you");
@@ -19,8 +19,8 @@ public class GenTest {
     }
 
     @Ignore
-    @Test(expected = Exception.class)
-    public void produceException() {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenLessArgumentsThanTemplateNeedThenExpectException() throws Exception {
         Generator generator = new Gen();
         String s = "I am a ${name}, Who are ${subt}? ";
         Map<String, String> map = Map.of("name", "Qwe", "subject", "you");
@@ -28,11 +28,20 @@ public class GenTest {
     }
 
     @Ignore
-    @Test(expected = Exception.class)
-    public void produceException2() {
+    @Test
+    public void whenMapContainsExcessKeyThenException() throws Exception {
         Generator generator = new Gen();
         String s = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> map = Map.of("name", "Qwe", "subject", "you", "www", "WWW");
+        assertEquals(generator.produce(s, map), "I am a Qwe, Who are you? ");
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException .class)
+    public void whenInvalidTemplateThenException() throws Exception {
+        Generator generator = new Gen();
+        String s = "I am a {name}, Who are ${subject}? ";
+        Map<String, String> map = Map.of("name", "Qwe", "subject", "you");
         assertEquals(generator.produce(s, map), "I am a Qwe, Who are you? ");
     }
 }
