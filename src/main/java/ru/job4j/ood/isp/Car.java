7c2f95629f664ae8fdeb51a76b68fc4dd27d5d4a
meavv <c.c.c.@bk.ru>
2021-11-05 15:07:59 +0500

    0. Принцип разделения интерфейсов [#4916 #223644]

---
+++ src/main/java/ru/job4j/ood/isp/Car.java
@@ -0,0 +1,8 @@
+package ru.job4j.ood.isp;
+
+public interface Car {
+
+    void run();
+    Car creation();
+
+}
---
+++ src/main/java/ru/job4j/ood/isp/Converter.java
@@ -0,0 +1,9 @@
+package ru.job4j.ood.isp;
+
+public interface Converter {
+
+    double rubToDollar(double rub);
+    double rubToEuro(double rub);
+
+
+}
